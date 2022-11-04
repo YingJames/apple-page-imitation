5b138ed69d9c273c079174850c5606722564b02b
James Yab <jyab0617@gmail.com>
2022-11-04 13:57:23 -0400

    modify shopping bag opacity when dropdown menu is open

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 isMenuOpen = false;
 let navMenu = document.body.querySelector('.nav--list');
 let navBar = document.body.querySelector('.nav__global');
+let shopBag = document.body.querySelector('.nav--bag-icn');
 
 let toggleMenu = () => {
     isMenuOpen = !isMenuOpen
     if (isMenuOpen == true) {
         navMenu.style.display = 'block';
         navBar.style.backgroundColor = 'rgba(0,0,0,1)';
+        shopBag.style.opacity = '0';
     } 
     else {
         navMenu.style.display = 'none';
         navBar.style.backgroundColor = 'rgba(0,0,0,0.8)';
+        shopBag.style.opacity = '1';
     }
 }
